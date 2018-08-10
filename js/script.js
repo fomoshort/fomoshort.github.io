5134df54475d9f759c7f3ac35c4b4e13ed8b0243
fomoshort <41759748+fomoshort@users.noreply.github.com>
2018-08-10 14:43:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -346,6 +346,7 @@ const main = function() {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
+    console.log(hours,mintues,seconds);	  
     return dateString = hours.toString() + " : " + minutes.toString() + " : " + seconds.toString();
   }	  
   async function updateTime(object) {
